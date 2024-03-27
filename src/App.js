005727d3fcb39e90fa56031764b5c0d2bb672fc2
Joshua10roys <joshuaashvinth@gmail.com>
2024-03-27 23:46:35 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,107 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import axios from 'axios';
 
 function App() {
   return (
     <div>
-      <FetchPdfFile />
-      <FileUploadForm />
+      <DownloadPdf />
+      <FileUpload />
+      {/* <FetchPdfFile /> */}
+      {/* <FileUploadForm /> */}
     </div>
   );
 }
 
 export default App;
+
+
+const DownloadPdf = () => {
+
+  const [loading, setLoading] = useState(false)
+  const [progress, setProgress] = useState(0)
+  const [file, setFile] = useState(null)
+
+  const getPdf = () => {
+    setLoading(true)
+    axios
+      .get("http://127.0.0.1:8000/file/getFile",
+        {
+          responseType: "blob",
+          onDownloadProgress: (progressEvent) => {
+            let percentage = Math.round((progressEvent.loaded * 100) / progressEvent.total)
+            setProgress(percentage)
+          }
+        })
+      .then((response) => {
+        const url = window.URL.createObjectURL(response.data);
+        setFile(url)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  };
+
+  return (
+    <div style={{ margin: "10px", height: "calc(50vh - 20px - 2px)", border: "1px solid gray" }}>
+      <h1 style={{ textAlign: "center" }}>Download</h1>
+
+      <div style={{ display: 'flex', flexDirection: "column", alignItems: 'center', gap: '10px' }}>
+        <div>
+          <button onClick={getPdf}>Download PDF</button>
+        </div>
+        <div>
+          {loading && <p>Downloading: {progress} %</p>}
+        </div>
+        <div>
+          {file && <a href={file} download={"example.pdf"} >Save</a>}
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+const FileUpload = () => {
+
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    // const formData = new FormData();
+    // formData.append("pdf", event.target.files[0])
+    // setFile(formData);
+    setFile(event.target.files[0])
+  };
+
+  const handleSubmit = async () => {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      await axios
+        .post("http://127.0.0.1:8000/file/uploadFile", formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+      console.log('File uploaded successfully');
+    } catch (error) {
+      console.error('Error uploading file:', error);
+    }
+  };
+
+  return (
+    <div style={{ margin: "10px", height: "calc(50vh - 20px - 2px)", border: "1px solid gray" }}>
+      <h1 style={{ textAlign: "center" }}>Upload</h1>
+      <div style={{ display: 'flex', flexDirection: "column", alignItems: 'center', gap: '10px' }}>
+        <input type="file" onChange={handleFileChange} />
+        <button onClick={handleSubmit}>Upload</button>
+      </div>
+    </div>
+  );
+}
 
 
 const FetchPdfFile = () => {
